45158d06d904e33af7a6ccf909d1b886f7776cae
anop <mhmdnvn18@gmail.com>
2025-08-21 11:30:38 +0700

    update

--- MuhammadNovian_12/10.HC-SR04/Tugas/Tugas.ino
+++ MuhammadNovian_12/10.HC-SR04/Tugas/Tugas.ino
@@ -21,6 +21,12 @@ void setup() {
   pinMode(ECHO, INPUT);
   pinMode(Buzzer, OUTPUT);
 
+  pinMode(led_1, OUTPUT);
+  pinMode(led_2, OUTPUT);
+  pinMode(led_3, OUTPUT);
+  pinMode(led_4, OUTPUT);
+
+
   lcd.begin();
   lcd.backlight();
   lcd.setCursor(0, 0);
@@ -45,6 +51,11 @@ void loop() {
   // Hitung jarak (cm)
   jarak = durasi * 0.034 / 2;
 
+  lcd.setCursor(0, 0);   // Or setting the cursor in the desired position.
+  lcd.print("Jarak: ");  // You can make spaces using well... spaces
+  lcd.setCursor(8, 1);   // Or setting the cursor in the desired position.
+  lcd.print(jarak);      // You can make spaces using well... spaces
+
   if (jarak > 50) {
     digitalWrite(led_1, 1);
     digitalWrite(led_2, 0);
@@ -65,11 +76,7 @@ void loop() {
     digitalWrite(led_3, 0);
     digitalWrite(led_4, 0);
 
-    lcd.setCursor(0, 0);   // Or setting the cursor in the desired position.
-    lcd.print("Jarak: ");  // You can make spaces using well... spaces
-    lcd.setCursor(8, 0);   // Or setting the cursor in the desired position.
-    lcd.print(jarak);      // You can make spaces using well... spaces
-    lcd.setCursor(1, 1);   // Or setting the cursor in the desired position.
+    lcd.setCursor(1, 1);  // Or setting the cursor in the desired position.
     lcd.print("Hati - Hati");
     delay(500);
 
@@ -83,8 +90,8 @@ void loop() {
     digitalWrite(led_3, 1);
     digitalWrite(led_4, 1);
 
-    lcd.setCursor(4, 0);  // Or setting the cursor in the desired position.
-    lcd.print("STOP !!");    // You can make spaces using well... spaces
+    lcd.setCursor(4, 1);   // Or setting the cursor in the desired position.
+    lcd.print("STOP !!");  // You can make spaces using well... spaces
     digitalWrite(Buzzer, 1);
     delay(500);
     digitalWrite(Buzzer, 0);
