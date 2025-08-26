85158ae5a28cb753d156d22fb44805848ffd9138
anop <mhmdnvn18@gmail.com>
2025-08-26 15:18:05 +0700

    upload

--- 2.ESP32/7.SoilMosture-Blynk_Kapasitif/7.SoilMosture-Blynk_Kapasitif/7.SoilMosture-Blynk_Kapasitif.ino
+++ 2.ESP32/7.SoilMosture-Blynk_Kapasitif/7.SoilMosture-Blynk_Kapasitif/7.SoilMosture-Blynk_Kapasitif.ino
@@ -1,10 +1,49 @@
-#define sensorPin 34  // ADC pin
-#define relayPin 4    // GPIO relay
+#define BLYNK_PRINT Serial
+#define BLYNK_TEMPLATE_ID "TMPL6muPlMtcI"
+#define BLYNK_TEMPLATE_NAME "BVPV Surakarta"
+#define BLYNK_AUTH_TOKEN "HEW65ui7sY0LkCDSX7G6s50JJ9NeJ9Vx"
+
+#include <LCD_I2C.h>
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <BlynkSimpleEsp32.h>
+#define relayPin 4
+#define sensorPin 34
+
+LCD_I2C lcd(0x27, 16, 2);
 bool pompaStatus = false;
+
+char ssid[] = "A33";
+char pass[] = "12345678";
+BlynkTimer timer;
+
 void setup() {
   Serial.begin(115200);
   pinMode(relayPin, OUTPUT);
   digitalWrite(relayPin, LOW);  // Pompa awal OFF
+
+  delay(10);
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, pass);
+  int wifi_ctr = 0;
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("WiFi connected");
+  Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
+
+  lcd.begin();
+  lcd.backlight();
+  lcd.setCursor(0, 0);
+  lcd.print("SoilMoisture TES");
+  lcd.setCursor(0, 1);
+  lcd.print(" BPVP Surakarta ");
+  delay(2000);
+  lcd.clear();
 }
 void loop() {
   int adcValue = analogRead(sensorPin);
@@ -25,4 +64,19 @@ void loop() {
   Serial.print("Pompa: ");
   Serial.println(pompaStatus ? "ON" : "OFF");
   delay(2000);
+
+  Serial.print("Nilai ADC: ");
+  Serial.print(sensorPin);
+  Serial.print(" | Kelembaban: ");
+  Serial.print(kelembaban);
+  Serial.println(" %");
+
+  lcd.setCursor(2, 0);
+  lcd.print("KELEMBABAN:   ");
+  lcd.setCursor(5, 1);
+  lcd.print(kelembaban, 1);
+  lcd.print("%   ");
+
+  Blynk.virtualWrite(V0, kelembaban);
+  Blynk.run();
 }
