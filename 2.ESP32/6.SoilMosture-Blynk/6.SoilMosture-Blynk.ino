0925ef86a235db4d1a607d4bfec74f6bf32313c0
anop <mhmdnvn18@gmail.com>
2025-08-26 11:20:22 +0700

    Add soil moisture monitoring with Blynk integration
    Introduces an ESP32-based soil moisture monitoring system using an analog sensor, LCD display, and relay control. Integrates with Blynk for remote monitoring and control, displaying real-time moisture data and automating relay based on threshold.

---
+++ 2.ESP32/6.SoilMosture-Blynk/6.SoilMosture-Blynk.ino
@@ -0,0 +1,70 @@
+#define BLYNK_PRINT Serial
+#define BLYNK_TEMPLATE_ID "TMPL6muPlMtcI"
+#define BLYNK_TEMPLATE_NAME "BVPV Surakarta"
+#define BLYNK_AUTH_TOKEN "HEW65ui7sY0LkCDSX7G6s50JJ9NeJ9Vx"
+
+#include <LCD_I2C.h>
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <BlynkSimpleEsp32.h>
+
+LCD_I2C lcd(0x27, 16, 2);
+
+char ssid[] = "A33";
+char pass[] = "12345678";
+
+#define relayPin 4
+#define sensorPin 34
+
+BlynkTimer timer;
+
+void bacaSensor() {
+  int sensorValue = analogRead(sensorPin);
+  float kelembaban = map(sensorValue, 0, 4095, 100, 0);
+
+  Blynk.virtualWrite(V0, kelembaban);
+
+  Serial.print("Nilai ADC: ");
+  Serial.print(sensorValue);
+  Serial.print(" | Kelembaban: ");
+  Serial.print(kelembaban);
+  Serial.println(" %");
+
+  lcd.setCursor(2, 0);
+  lcd.print("KELEMBABAN:   ");
+  lcd.setCursor(5, 1);
+  lcd.print(kelembaban, 1);
+  lcd.print("%   ");
+
+  if (kelembaban < 10) {
+    digitalWrite(relayPin, LOW);  // active LOW
+    Serial.println("NYALA");
+  } else {
+    digitalWrite(relayPin, HIGH);
+    Serial.println("MATI");
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+
+  Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
+
+  lcd.begin();
+  lcd.backlight();
+  lcd.setCursor(0, 0);
+  lcd.print("SoilMoisture TEs");
+  lcd.setCursor(0, 1);
+  lcd.print(" BPVP Surakarta ");
+  delay(2000);
+  lcd.clear();
+
+  pinMode(relayPin, OUTPUT);
+
+  timer.setInterval(1000L, bacaSensor);
+}
+
+void loop() {
+  Blynk.run();
+  timer.run();
+}
